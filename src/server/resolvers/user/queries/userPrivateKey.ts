a5c8b8c129dda21fb5827ad98cf5fbf3071017f0
shubhamkakkar <shubham.kakkar998@gmail.com>
2019-11-13 02:19:08 +0530

    code modification done, to be checked and tested

--- src/server/resolvers/user/queries/userPrivateKey.ts
+++ src/server/resolvers/user/queries/userPrivateKey.ts
@@ -1,14 +1,24 @@
 import UserSchema from "../../../models/user/user";
 import {JWTVerify} from "../jwt";
+import {decrypted, verified} from "../../block/customHelperFunctions";
 
 
-export default function userPrivateKeyQuery(token:string) {
+export default function userPrivateKeyQuery(token: string) {
 
     // @ts-ignore
     const {email: {email}} = JWTVerify(token);
     return UserSchema.findOne({email})
         .then(user => {
+            if (user) {
+                // @ts-ignore
+                const {privateKey} = user._doc;
+                return decrypted({privatekey: privateKey, encrypted: privateKey})
+                    .then(({message}: { message: string }) => message)
+                    .catch(er => console.log("failed in dcryption of privateKey in user"))
+            } else {
+                console.log("user not found - user private key")
+            }
 
         })
-        .catch(er => console.log("user not found - user private key"))
+        .catch(er => console.log("user er found - user private key"))
 }
