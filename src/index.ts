c4607b99c8ed0f71b9252870cbcb2a63b4e0da6d
shubhamkakkar <shubham.kakkar998@gmail.com>
2019-11-14 03:54:37 +0530

    heroku work started

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import schema from './server/schema';
 import resolvers from './server/resolvers'
 
 config({path: resolve(__dirname, "../.env")});
-const {MONGO_URI_DEV} = process.env;
+const {MONGO_URI_DEV, PORT} = process.env;
 // @ts-ignore
 mongoose.connect(MONGO_URI_DEV, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true})
     .then((res: any) => {
@@ -21,8 +21,11 @@ mongoose.connect(MONGO_URI_DEV, {useNewUrlParser: true, useFindAndModify: false,
         const app = express();
         app.use(cors(), bodyParser.json());
         server.applyMiddleware({app});
-        app.listen({port: 4000}, () =>
-            console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+
+        const port = PORT || 4000;
+        app.listen({port}, () => {
+                console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+            }
         );
     })
     .catch((er: any) => console.log("failed to connect to mongoose", er));
