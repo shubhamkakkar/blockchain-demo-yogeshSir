78f8964d43bd03b25e7e5069090ced81de14737e
shubhamkakkar <shubham.kakkar998@gmail.com>
2019-11-16 18:11:55 +0530

    .js and dist removed

--- dist/generated/graphql.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CacheControlScope;
-(function (CacheControlScope) {
-    CacheControlScope["Public"] = "PUBLIC";
-    CacheControlScope["Private"] = "PRIVATE";
-})(CacheControlScope = exports.CacheControlScope || (exports.CacheControlScope = {}));
-//# sourceMappingURL=graphql.js.map
--- dist/server/resolvers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_1 = require("./user");
-const block_1 = require("./block");
-exports.default = [user_1.default, block_1.default];
-//# sourceMappingURL=index.js.map
